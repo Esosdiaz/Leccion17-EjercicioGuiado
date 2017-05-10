8d438b63bff6ed67d173c670c50fab601e14a44c
Fa Diaz <diazgonzalez.perla@gmail.com>
2017-05-09 23:27:10 -0400

    segundo ejercicio terminado

---
+++ ejercicio2.js
@@ -0,0 +1,42 @@
+
+
+var productos = [
+   {
+	nombre: 'pijama',
+	tipo: 'ropa'
+},
+   {
+	nombre: 'nevera',
+	tipo: 'electrodomestico'
+},
+   {
+	nombre: 'asus',
+	tipo: 'computador'
+},
+   {
+	nombre: 'macbook',
+	tipo: 'computador'
+},
+   {
+	nombre: 'pijama',
+	tipo: 'ropa'
+},
+   {
+	nombre: 'toshiba',
+	tipo: 'computador'
+},
+];
+
+var computadores = [];
+for(var i=0; i<productos.length; i++){
+	if (productos[i].tipo === 'computador'){
+		computadores.push(productos[i]);
+	}
+}
+console.log(computadores)
+
+var computadores = productos.filter(function(producto){
+	return producto.tipo === 'computador';
+});
+console.log(computadores);
+
